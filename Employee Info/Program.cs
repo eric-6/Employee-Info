5c51bd30b3e34a4a023895e37008cfa4996aab3b
eric-6 <nyaeric237@gmail.com>
2021-02-28 22:44:48 -0500

    Add project files.

---
+++ Employee Info/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Employee_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string birthdate;
+            string extension;
+            string email_address;
+
+            Console.WriteLine("Enter employees name ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter employess birthdate");
+            birthdate = Console.ReadLine();
+
+            Console.WriteLine("Enter employess extension");
+            extension = Console.ReadLine();
+
+            Console.WriteLine("Enter employess email address");
+            email_address = Console.ReadLine();
+
+
+
+        }
+    }
+}
